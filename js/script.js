61a213e7aaa88513e04fe710145dd85a12417535
Gbozer <166019196+Gbozer@users.noreply.github.com>
2024-04-15 01:04:23 +0100

    my forth commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("myform");
+const form = document.getElementById("myForm");
 const nameInput = document.getElementById("name");
 const nameError = document.getElementById("nameError");
 const emailInput = document.getElementById("email");
@@ -13,11 +13,21 @@ form.addEventListener("submit",function(event){
     emailError.textContent = "";
 
     if(nameInput.value.trim()===""){
-        nameError.textContent = "Please correct errors on form!";
+        nameError.textContent = "Hey what is your name?!";
         valid = false;
 
         setTimeout(()=>{
             nameError.textContent ="";
+        },3000);
+    }
+
+    const emailPartter = /@.*\.com/;
+    if (!emailParttern.test(emailInput)){
+        emailError.textContent ="Hey check your email";
+        valid = failse;
+
+        setTimeout(() =>{
+            emailError.textContent="";
         },3000);
     }
 })
