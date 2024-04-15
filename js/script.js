f83f72cf52b9d26a7d5fb336a6259e5e860428a3
Gbozer <166019196+Gbozer@users.noreply.github.com>
2024-04-15 19:19:56 +0100

    my eightieth commit

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ const nameError = document.getElementById("nameError");
 const emailInput = document.getElementById("email");
 const emailError= document.getElementById("emailError");
 
-form.addEventListener("submit",function(event){
+form.addEventListener("submit", function(event){
     event.preventDefault();
-
+    
     let valid = true;
-
+    
     nameError.testContent = "";
     emailError.textContent = "";
 
-    if(nameInput.value.trim()===""){
-        nameError.textContent = "Hey what is your name?!";
+    if(nameInput.value.trim() === ""){
+        nameError.textContent ="Hey what is your name?";
         valid = false;
 
         setTimeout(()=>{
-            nameError.textContent ="";
+            nameError.textContent = "";
         },3000);
     }
 
-    const emailPartter = /@.*\.com/;
+    const emailParttern= /@.*\.com/;
     if (!emailParttern.test(emailInput)){
         emailError.textContent ="Hey check your email";
         valid = failse;
